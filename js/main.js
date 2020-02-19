9205009fe60520c96d8b8189d66bf239b470d555
Zahari Dimitrov <z.dimitrov98@gmail.com>
2020-02-19 15:56:49 +0100

    Novi neshta

--- js/main.js
+++ js/main.js
@@ -60,30 +60,145 @@ function dragElement(elmnt) {
 //}
 
 function toggle1() {
- if( document.getElementById("hidethis_dots").style.display=='none' ){
-   document.getElementById("hidethis_dots").style.display = 'table-row';
+ if( document.getElementById("hidethis_one").style.display=='none' ){
+   document.getElementById("hidethis_one").style.display = 'table-row';
 // set to table-row instead of an empty string
  } else {
-   document.getElementById("hidethis_dots").style.display = 'none';
+   document.getElementById("hidethis_one").style.display = 'none';
  }
 }
 
 function toggle2() {
- if( document.getElementById("hidethis_olx").style.display=='none' ){
-   document.getElementById("hidethis_olx").style.display = 'table-row';
+ if( document.getElementById("hidethis_two").style.display=='none' ){
+   document.getElementById("hidethis_two").style.display = 'table-row';
 // set to table-row instead of an empty string
  }else{
-   document.getElementById("hidethis_olx").style.display = 'none';
+   document.getElementById("hidethis_two").style.display = 'none';
  }
 }
 
 function toggle3() {
- if( document.getElementById("hidethis_metro").style.display=='none' ){
-   document.getElementById("hidethis_metro").style.display = 'table-row';
+ if( document.getElementById("hidethis_three").style.display=='none' ){
+   document.getElementById("hidethis_three").style.display = 'table-row';
 // set to table-row instead of an empty string
  }else{
-   document.getElementById("hidethis_metro").style.display = 'none';
+   document.getElementById("hidethis_three").style.display = 'none';
  }
 }
 
+function toggle4() {
+ if( document.getElementById("hidethis_four").style.display=='none' ){
+   document.getElementById("hidethis_four").style.display = 'table-row';
+// set to table-row instead of an empty string
+ }else{
+   document.getElementById("hidethis_four").style.display = 'none';
+ }
+}
 
+function toggle5() {
+ if( document.getElementById("hidethis_five").style.display=='none' ){
+   document.getElementById("hidethis_five").style.display = 'table-row';
+// set to table-row instead of an empty string
+ }else{
+   document.getElementById("hidethis_five").style.display = 'none';
+ }
+}
+
+function toggle6() {
+ if( document.getElementById("hidethis_six").style.display=='none' ){
+   document.getElementById("hidethis_six").style.display = 'table-row';
+// set to table-row instead of an empty string
+ }else{
+   document.getElementById("hidethis_six").style.display = 'none';
+ }
+}
+
+function toggle7() {
+ if( document.getElementById("hidethis_seven").style.display=='none' ){
+   document.getElementById("hidethis_seven").style.display = 'table-row';
+// set to table-row instead of an empty string
+ }else{
+   document.getElementById("hidethis_seven").style.display = 'none';
+ }
+}
+
+function toggle8() {
+ if( document.getElementById("hidethis_eight").style.display=='none' ){
+   document.getElementById("hidethis_eight").style.display = 'table-row';
+// set to table-row instead of an empty string
+ }else{
+   document.getElementById("hidethis_eight").style.display = 'none';
+ }
+}
+
+function toggle9() {
+ if( document.getElementById("hidethis_nine").style.display=='none' ){
+   document.getElementById("hidethis_nine").style.display = 'table-row';
+// set to table-row instead of an empty string
+ }else{
+   document.getElementById("hidethis_nine").style.display = 'none';
+ }
+}
+
+function toggle10() {
+ if( document.getElementById("hidethis_ten").style.display=='none' ){
+   document.getElementById("hidethis_ten").style.display = 'table-row';
+// set to table-row instead of an empty string
+ }else{
+   document.getElementById("hidethis_ten").style.display = 'none';
+ }
+}
+
+function toggle11() {
+ if( document.getElementById("hidethis_eleven").style.display=='none' ){
+   document.getElementById("hidethis_eleven").style.display = 'table-row';
+// set to table-row instead of an empty string
+ }else{
+   document.getElementById("hidethis_eleven").style.display = 'none';
+ }
+}
+
+function toggle12() {
+ if( document.getElementById("hidethis_twelve").style.display=='none' ){
+   document.getElementById("hidethis_twelve").style.display = 'table-row';
+// set to table-row instead of an empty string
+ }else{
+   document.getElementById("hidethis_twelve").style.display = 'none';
+ }
+}
+
+function toggle13() {
+ if( document.getElementById("hidethis_thirteen").style.display=='none' ){
+   document.getElementById("hidethis_thirteen").style.display = 'table-row';
+// set to table-row instead of an empty string
+ }else{
+   document.getElementById("hidethis_thirteen").style.display = 'none';
+ }
+}
+
+function toggle14() {
+ if( document.getElementById("hidethis_forteen").style.display=='none' ){
+   document.getElementById("hidethis_forteen").style.display = 'table-row';
+// set to table-row instead of an empty string
+ }else{
+   document.getElementById("hidethis_forteen").style.display = 'none';
+ }
+}
+
+function toggle15() {
+ if( document.getElementById("hidethis_fifteen").style.display=='none' ){
+   document.getElementById("hidethis_fifteen").style.display = 'table-row';
+// set to table-row instead of an empty string
+ }else{
+   document.getElementById("hidethis_fifteen").style.display = 'none';
+ }
+}
+
+function toggle16() {
+ if( document.getElementById("hidethis_sixteen").style.display=='none' ){
+   document.getElementById("hidethis_sixteen").style.display = 'table-row';
+// set to table-row instead of an empty string
+ }else{
+   document.getElementById("hidethis_sixteen").style.display = 'none';
+ }
+}
